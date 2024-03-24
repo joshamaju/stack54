9183233377bbf4fa115437ce6d711ec70d4c2e81
Joshua Amaju <princejosh0007@gmail.com>
2024-03-24 14:23:01 +0100

    chore

---
+++ packages/core/src/runtime/middlewares/views.ts
@@ -0,0 +1,15 @@
+import type { Context, MiddlewareHandler } from "hono";
+import { makeFactory } from "../render.js";
+
+const key = "$$render";
+
+export const view =
+  (fn: ReturnType<typeof makeFactory>): MiddlewareHandler =>
+  async (ctx, next) => {
+    ctx.set(key, (...args: Parameters<typeof fn>) => fn(...args));
+    await next();
+  };
+
+export const getRenderFn = (ctx: Context) => {
+  return ctx.get(key) as ReturnType<typeof makeFactory>;
+};
