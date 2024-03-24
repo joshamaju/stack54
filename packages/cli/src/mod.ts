bbffcbc0f7ca25b80ea8d64977a3fc12018fca80
Joshua Amaju <princejosh0007@gmail.com>
2024-03-24 13:34:48 +0100

    feat: add view generator

--- packages/cli/src/mod.ts
+++ packages/cli/src/mod.ts
@@ -1,8 +1,10 @@
 import fs from "node:fs";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 import sade from "sade";
 import { create_app } from "./commands/create-app.js";
+import { create_controller, create_views } from "./commands/generate/mod.js";
 
 const pkg_file = fs.readFileSync(
   fileURLToPath(new URL("../package.json", import.meta.url)),
@@ -18,5 +20,31 @@ program
   .describe("Bootstrap a new project")
   .option("-t, --template", "Use template", "basic")
   .action((name, opts) => create_app({ name, template: opts.template }));
+
+program
+  .command("make <type> <name>")
+  .option("-v, --views", "Views directory to use")
+  .option("-c, --controllers", "Controllers directory to use")
+  .option("-p, --param", "Route param to use instead of the default :id")
+  .action((type, name, opts) => {
+    const cwd = process.cwd();
+
+    switch (type) {
+      case "controller": {
+        const dir = opts.controllers ? path.join(cwd, opts.controllers) : cwd;
+        create_controller({ name, directory: dir });
+        break;
+      }
+
+      case "view": {
+        const dir = opts.views ? path.join(cwd, opts.views) : cwd;
+        create_views({ name, directory: dir });
+        break;
+      }
+
+      default:
+        break;
+    }
+  });
 
 program.parse(process.argv);
