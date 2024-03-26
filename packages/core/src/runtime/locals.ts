20edbce59a454ee9f84a505a0d392ae8595a85b4
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2024-03-26 12:18:24 +0100

    Middlewares/views (#5)
    * feat: add view rendering middle ware
    
    * chore: add locals helpers
    
    * feat: type hono context render function
    
    ---------
    
    Co-authored-by: Joshua Amaju <princejosh0007@gmail.com>

---
+++ packages/core/src/runtime/locals.ts
@@ -0,0 +1,32 @@
+import type { Context } from "hono";
+
+export const key = "$$local";
+
+export function setLocals<T extends App.Locals, K extends keyof T>(
+  ctx: Context,
+  name: K,
+  value: T[K]
+): void;
+export function setLocals(ctx: Context, name: Partial<App.Locals>): void;
+export function setLocals<T extends App.Locals, K extends keyof T>(
+  ctx: Context,
+  name: K | Partial<T>,
+  value?: T[K]
+): void {
+  const locals = (ctx.get(key) ?? {}) as App.Locals;
+
+  ctx.set(
+    key,
+    typeof name == "object"
+      ? { ...locals, ...name }
+      : { ...locals, [name]: value }
+  );
+}
+
+// For getting locals inside server handlers
+export function getLocals(ctx: Context): App.Locals;
+export function getLocals(ctx: Context, name: string): unknown;
+export function getLocals(ctx: Context, name?: string) {
+  const locals = structuredClone(ctx.get(key) ?? {});
+  return name ? locals[name] : locals;
+}
