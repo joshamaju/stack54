cb38cff54e75d8ccab25cc4052e7cf9cd0788e75
Joshua Amaju <princejosh0007@gmail.com>
2024-04-01 22:05:22 +0100

    chore

--- templates/basic/vite.config.ts
+++ templates/basic/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import fullstack from "stack54/vite";
+import plugin from "stack54/vite";
 
 export default defineConfig({
-  plugins: [fullstack()],
+  plugins: [plugin()],
 });
