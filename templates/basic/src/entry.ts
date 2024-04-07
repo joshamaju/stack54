6f14faf38a9e90d989e4178c640fb1517348b645
Joshua Amaju <princejosh0007@gmail.com>
2024-04-07 13:39:05 +0100

    chore: clean up

--- templates/basic/src/entry.ts
+++ templates/basic/src/entry.ts
@@ -1,26 +1,12 @@
 import { Hono } from "hono";
-import { VERSION } from "svelte/compiler";
 import { view } from "stack54/view";
 
-import { $ } from "execa";
-
 import { render } from "./utils/view";
-import { dependencies } from "../package.json";
 
 const app = new Hono();
 
 app.use(view(render));
 
-app.get("/", async (ctx) => {
-  const node = await $`node -v`;
-
-  return ctx.render("welcome", {
-    versions: {
-      node: node.stdout,
-      svelte: `v${VERSION}`,
-      stack54: dependencies.stack54,
-    },
-  });
-});
+app.get("/", (ctx) => ctx.render("welcome"));
 
 export default app;
--- templates/basic/src/views/welcome.svelte
+++ templates/basic/src/views/welcome.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import Document from "./components/document.svelte";
-  export let versions: { stack54: string; node: string; svelte: string };
 </script>
 
 <Document>
   <svelte:fragment slot="head">
+    <title>stack54</title>
     <link rel="stylesheet" href="./welcome.css" />
   </svelte:fragment>
 
@@ -13,12 +13,6 @@
       <h1>
         <img src="/stack54.png" alt="stack54" />
       </h1>
-
-      <div class="versions">
-        <p>stack54 - <span>{versions.stack54}</span></p>
-        <p>svelte - <span>{versions.svelte}</span></p>
-        <p>node - <span>{versions.node}</span></p>
-      </div>
     </main>
   </section>
 </Document>
@@ -40,27 +34,5 @@
 
   img {
     width: 20rem;
-  }
-
-  .versions {
-    display: flex;
-    align-items: center;
-  }
-
-  .versions > * {
-    padding: 0 2rem;
-  }
-
-  .versions > * + * {
-    border-left: 1px solid #ccc;
-  }
-
-  p {
-    font-weight: bold;
-  }
-
-  span {
-    font-style: italic;
-    font-weight: 400;
   }
 </style>
