91634ee77d7f4ae9fccaa00cea72875744ca36b4
Joshua Amaju <princejosh0007@gmail.com>
2024-08-07 11:38:21 +0100

    chore

---
+++ packages/core/components/Await/make.ts
@@ -0,0 +1,26 @@
+import type { ComponentType } from "svelte";
+
+import { await as awaitter } from "stack54/render/streaming";
+import Await from "./Await.svelte";
+
+export function make<
+  P extends Record<string, any>,
+  T extends ComponentType<Await<P>>
+>(Component: T): T {
+  // @ts-expect-error
+  if (Component.$$render) {
+    // @ts-expect-error
+    Component.$$render = function (
+      result: any,
+      props: any,
+      bindings: any,
+      slots: any
+    ) {
+      return awaitter({ slots, resolve: props.resolve });
+    };
+
+    return Component;
+  }
+
+  return Component;
+}
