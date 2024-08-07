c9bb7d23d270c6ddb224c3b5ea87113fccd80eb8
Joshua Amaju <princejosh0007@gmail.com>
2024-08-07 11:38:56 +0100

    chore

---
+++ packages/core/components/Await/Await.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  type T = $$Generic<T>;
+
+  interface $$Slots {
+    error: { error: Error };
+    default: { value: T };
+    fallback: any;
+  }
+
+  // @ts-expect-error
+  export const resolve: PromiseLike<T> = null;
+  const error = null as any as Error;
+  const value = null as any as T;
+</script>
+
+<slot name="error" {error} />
+<slot name="fallback" />
+<slot {value} />
