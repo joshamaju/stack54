cc723d8c16cf6a431c05fbaf9a9ab8f9ed7aa521
Joshua Amaju <princejosh0007@gmail.com>
2024-08-28 11:58:15 +0100

    chore: destroy component

--- integrations/island/src/hydrate.ts
+++ integrations/island/src/hydrate.ts
@@ -1,16 +1,23 @@
+import type { SvelteComponent } from "svelte";
 import type { Callback } from "./directives/types.js";
 
 export function hydrate(load: Callback) {
   class Island extends HTMLElement {
+    private instance?: SvelteComponent<any, any, any>;
+
     connectedCallback() {
+      const target = this;
+
       load((Component) => {
         import("stack54/data").then(({ raw_decode }) => {
           const props = raw_decode(this.getAttribute("props")!) as any;
-          console.log(props);
-          new Component({ props, target: this, hydrate: true });
-          const event = new Event("hydrate");
+          this.instance = new Component({ props, target, hydrate: true });
         });
-      }, this);
+      }, target);
+    }
+
+    disconnectedCallback() {
+      this.instance?.$destroy();
     }
   }
 
