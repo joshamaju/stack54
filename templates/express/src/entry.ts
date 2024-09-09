b46cf55daedd8510b0b3b3cfc2d187e7c8c177d8
Joshua Amaju <princejosh0007@gmail.com>
2024-09-09 11:41:16 +0100

    update starter template

--- templates/express/src/entry.ts
+++ templates/express/src/entry.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import methodOverride from "method-override";
+
 import { register } from "@stack54/express/render";
 
 import { render } from "./utils/view";
@@ -6,6 +10,23 @@ import { render } from "./utils/view";
 const app = express();
 
 register(app, render);
+
+app.use(methodOverride());
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: import.meta.env.SESSION_SECRET,
+    cookie: { secure: import.meta.env.PROD },
+  })
+);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/", (_, res) => {
   return res.render("welcome", {});
