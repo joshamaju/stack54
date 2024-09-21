c4013aa2c6f3d9acb7d3b69acee8185db8c617f9
Joshua Amaju <joshsemail4work@gmail.com>
2024-09-21 19:34:24 +0100

    feat: support islands having slots, including named slots

---
+++ packages/core/test/apps/basic/src/views/island/with-slot.page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Document from "../components/document.svelte";
+  import WithSlot from "./with-slot.svelte";
+</script>
+
+<Document>
+  <WithSlot>
+    <div>
+      <p>default</p>
+    </div>
+
+    <div slot="named">
+      <p>named</p>
+    </div>
+  </WithSlot>
+</Document>
