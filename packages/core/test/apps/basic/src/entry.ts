4de1d638be120745406cf8e3ff2bc8ade431d388
Joshua Amaju <joshsemail4work@gmail.com>
2024-09-22 00:07:20 +0100

    chore

--- packages/core/test/apps/basic/src/entry.ts
+++ packages/core/test/apps/basic/src/entry.ts
@@ -33,7 +33,6 @@ app.get("/env/client", (_, res) => res.render("env/client"));
 app.get("/env/public", (_, res) => res.render("env/public"));
 app.get("/env/in-view", (_, res) => res.render("env/in-view"));
 app.get("/env/server", (_, res) => {
-  // @ts-expect-error
   return res.render("env/server", { env: import.meta.env.ENV });
 });
 
