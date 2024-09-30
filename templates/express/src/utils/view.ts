6ded0c1ee48f01cf74b5264017902f19f92d59da
Joshua Amaju <joshsemail4work@gmail.com>
2024-09-30 13:54:31 +0100

    make template resolution faster in dev mode

--- templates/express/src/utils/view.ts
+++ templates/express/src/utils/view.ts
@@ -1,9 +1,7 @@
 import { makeFactory, resolveComponent } from "stack54/render";
 import { type TemplateModule } from "stack54/types";
 
-const components = import.meta.glob<TemplateModule>("../views/**/*.svelte", {
-  eager: true,
-});
+const components = import.meta.glob<TemplateModule>("../views/**/*.svelte");
 
 export const render = makeFactory((name) => {
   return resolveComponent(`../views/${name}.svelte`, components);
