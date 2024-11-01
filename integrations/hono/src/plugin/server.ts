dda795468a1f0b3567772a73c7063bb7b9aa45cf
Joshua Amaju <joshsemail4work@gmail.com>
2024-11-01 14:39:04 +0100

    chore(integrations): remove preview server

--- integrations/hono/src/plugin/server.ts
+++ integrations/hono/src/plugin/server.ts
@@ -1,11 +1,8 @@
-import path from "node:path";
-import { pathToFileURL } from "node:url";
-import type { PreviewServer, ViteDevServer } from "vite";
 import type { Hono } from "hono";
-import sirv from "sirv";
+import type { ViteDevServer } from "vite";
 
-import { installPolyfills, shouldPolyfill } from "./polyfill.js";
 import { getRequest, setResponse } from "../request.js";
+import { installPolyfills, shouldPolyfill } from "./polyfill.js";
 
 const error_template = /*html*/ `
 <html>
@@ -66,40 +63,5 @@ export function devServer(server: ViteDevServer, opts: { entry: string }) {
     } else {
       setResponse(res, response);
     }
-  });
-}
-
-export async function previewServer(server: PreviewServer) {
-  if (shouldPolyfill) {
-    installPolyfills();
-  }
-
-  const protocol = server.config.preview.https ? "https" : "http";
-
-  const publicDir = server.config.publicDir;
-  const { outDir, assetsDir } = server.config.build;
-
-  const module = await import(
-    pathToFileURL(path.join(outDir, "index.js")).href
-  );
-
-  const app: Hono = module.default;
-
-  server.middlewares.use(
-    sirv(publicDir, {
-      setHeaders: (res, pathname) => {
-        // only apply to build output directory
-        if (pathname.startsWith(`/${assetsDir}`)) {
-          res.setHeader("cache-control", "public,max-age=31536000,immutable");
-        }
-      },
-    })
-  );
-
-  server.middlewares.use(async (req, res) => {
-    const host = req.headers["host"];
-    const request = getRequest({ request: req, base: `${protocol}://${host}` });
-    const response = await app.fetch(request);
-    setResponse(res, response);
   });
 }
