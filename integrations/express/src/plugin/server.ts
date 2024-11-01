d55c9aab5db29385d574bdec90da97c798e40acb
Joshua Amaju <joshsemail4work@gmail.com>
2024-11-01 14:41:14 +0100

    chore

--- integrations/express/src/plugin/server.ts
+++ integrations/express/src/plugin/server.ts
@@ -1,9 +1,4 @@
-import path from "node:path";
-import { pathToFileURL } from "node:url";
-
-import type { Express } from "express";
-import sirv from "sirv";
-import type { Connect, PreviewServer, ViteDevServer } from "vite";
+import type { Connect, ViteDevServer } from "vite";
 
 export async function devServer(
   server: ViteDevServer,
@@ -21,28 +16,4 @@ export async function devServer(
   // server.middlewares.stack.unshift({ handle, route: "" });
 
   server.middlewares.use(handle);
-}
-
-export async function previewServer(server: PreviewServer) {
-  const publicDir = server.config.publicDir;
-  const { outDir, assetsDir } = server.config.build;
-
-  const module = await import(
-    pathToFileURL(path.join(outDir, "index.js")).href
-  );
-
-  const app: Express = module.default;
-
-  server.middlewares.use(
-    sirv(publicDir, {
-      setHeaders: (res, pathname) => {
-        // only apply to build output directory
-        if (pathname.startsWith(`/${assetsDir}`)) {
-          res.setHeader("cache-control", "public,max-age=31536000,immutable");
-        }
-      },
-    })
-  );
-
-  server.middlewares.use(app);
 }
