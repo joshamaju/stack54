e72c5c4bcfc0ef2c2f2ba7ea58939a2607dec392
Joshua Amaju <joshsemail4work@gmail.com>
2025-03-16 22:57:50 +0100

    chore

--- packages/core/test/apps/basic/stack.config.js
+++ packages/core/test/apps/basic/stack.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "stack54/config";
 import express from "@stack54/express/plugin";
+import island from "@stack54/island"
 
 export default defineConfig({
-  integrations: [express()],
-  views: ["src/views/**/*.svelte"],
+  integrations: [express(), island()],
 });
