d60b59d5356193436cf4fd3cb899ace3611c14e3
Joshua Amaju <joshsemail4work@gmail.com>
2025-03-16 21:27:00 +0100

    chore

--- packages/core/test/apps/basic/stack.config.js
+++ packages/core/test/apps/basic/stack.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "stack54/config";
 import express from "@stack54/express/plugin";
-import island from "@stack54/island"
 
 export default defineConfig({
-  integrations: [express(), island()],
+  integrations: [express()],
 });
