088102b937530e77e078762a16f3fd9a8e784537
Joshua Amaju <joshsemail4work@gmail.com>
2025-03-22 18:21:09 +0100

    chore

--- packages/core/test/apps/basic/src/views/env/hydrated/index.svelte
+++ packages/core/test/apps/basic/src/views/env/hydrated/index.svelte
@@ -1,22 +1,31 @@
 <script>
-  import Document from "../../components/document.svelte";
   import Component from "./component.svelte";
 </script>
 
-<Document>
-  <script slot="head" type="module">
-    import Component from "./component.svelte";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
+    <button data-testid="hydrate">hydrate</button>
 
-    const btn = document.querySelector('[data-testid="hydrate"]');
+    <div id="app">
+      <Component />
+    </div>
 
-    btn.addEventListener("click", () => {
-      new Component({ hydrate: true, target: document.getElementById("app") });
-    });
-  </script>
+    <script type="module">
+      import Component from "./component.svelte";
 
-  <button data-testid="hydrate">hydrate</button>
+      const btn = document.querySelector('[data-testid="hydrate"]');
 
-  <div id="app">
-    <Component />
-  </div>
-</Document>
+      btn.addEventListener("click", () => {
+        new Component({
+          hydrate: true,
+          target: document.getElementById("app"),
+        });
+      });
+    </script>
+  </body>
+</html>
