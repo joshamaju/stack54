6e28503f78197bb013aa856de644ce044698b115
Joshua Amaju <joshsemail4work@gmail.com>
2025-12-27 14:18:37 +0100

    chore

--- packages/core/scripts/version.js
+++ packages/core/scripts/version.js
@@ -1,5 +1,5 @@
 import * as fs from "node:fs"
-import pkg from "../package.json" assert { type: "json" }
+import pkg from "../package.json" with { type: "json" }
 
 fs.writeFileSync(
   "./version.js",
