2892c2ba46b19d13b09b9f8bf98ceec462973191
Joshua Amaju <joshsemail4work@gmail.com>
2026-01-03 17:15:34 +0100

    chore: update remote functions example

--- examples/with-remote-function/bin/server.js
+++ examples/with-remote-function/bin/server.js
@@ -4,7 +4,7 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 
-import router from "../dist/server/index.js";
+import router from "../dist/server/entry.js";
 
 const app = new Hono();
 
